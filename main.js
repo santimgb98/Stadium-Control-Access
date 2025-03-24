9ba2b5d717aca64d26368cd1f72b280a3478ea26
santi <santimgb98@gmail.com>
2025-03-24 22:48:16 +0100

    add st.zones

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
 // opción para ajustar el aforo
 
 /* INTERFAZ DE LA CONSOLA 
-Menú interactivo donde el urusario pueda:
+Menú interactivo donde el usuario pueda:
 - comprar boletos (ingresar los datos de la persona)
 - verificar acceso (ingresar el codigo del  boleto)
 - registrar ingreso (confirmar el acceso y liberar el asiento cuandos se salga)
@@ -29,15 +29,16 @@ const ruta = "./data.json";
 
 const datas = JSON.parse(fs.readFileSync(ruta, "utf8"));
 
-const max_capacity = datas[0].max_capacity;
-const current_capacity = datas[1].current_capacity;
+const max_capacity = datas[0];
+const current_capacity = datas[1];
 const tickets_array = datas[2];
+console.log(max_capacity);
+console.log(current_capacity);
 
+// Para generar un ticket de 3 dígitos de código, que con 37 opciones esta aproximación tiene poco más de 50k opciones
 const ticket_generator = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-    'u', 'v', 'w', 'x', 'y', 'z', 
-    '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 'z', '0' ,'1', '2', '3', '4', '5', '6', '7', '8', '9', '#'
 ];
 
 
@@ -48,11 +49,31 @@ class stadium_capacity{
         this.seat = seat;
     }
 
-    BuyTickets(){
+BuyTickets(){
 
         console.clear();
         this.name = prompt("Enter name: ");
-        this.seat = Math.floor(Math.random()*50000);
+
+        // Selección de zona del estadio
+        console.log("SELECTION STADIUM ZONE: ");
+        console.log("1. VIP zone");
+        console.log("2. North zone");
+        console.log("3. South zone");
+        console.log("4. West zone");
+        console.log("5. East zone");
+        let zone_selection = prompt("Select area you want: ");
+
+        while(isNaN(zone_selection)=== true || zone_selection < 1 || zone_selection > 5){
+            zone_selection = prompt("Select a valid area please: ");
+        }
+        switch(zone_selection){
+            case "1":{this.seat = Math.floor(Math.random()*100) + ", on VIP zone";}break;
+            case "2":{this.seat = Math.floor(Math.random()*(20000-100 + 1) + 100) + ", on North zone";}break;
+            case "3":{this.seat = Math.floor(Math.random()*(30000-20000 + 1) + 20000) + ", on South zone";}break;
+            case "4":{this.seat = Math.floor(Math.random()*(40000-30000 + 1) + 30000) + ", on West zone";}break;
+            case "5":{this.seat = Math.floor(Math.random()*(50000-40000 + 1) + 40000) + ", on East zone";}break;
+        }
+
         console.clear();
 
         // Función para barajar el array de todos los dígitos posibles para el código del billete
@@ -68,23 +89,30 @@ class stadium_capacity{
             }
         
             const random_array = [];
-                random_array.push(array[1]); // 5 dígitos aleatorios del nuevo array barajado
+                random_array.push(array[1]); // 3 dígitos aleatorios del nuevo array barajado
                 random_array.push(array[2]);
                 random_array.push(array[3]);
-                random_array.push(array[4]);
-                random_array.push(array[5]);
 
-            // Elimina las "," del array donde mete los 5 dígitos aleatorios,
+            // Elimina las "," del array donde mete los 3 dígitos aleatorios,
             // creando así el nuevo ticket
-            const ticket_array = (random_array.toString()).replace(/,/g,"");
+            const newCod = (random_array.toString()).replace(/,/g,"");
         
-            return ticket_array;
+            return newCod;
         }
         
         this.ticket = shuffle(ticket_generator);
+        while(tickets_array.includes(this.ticket)){
+            this.ticket = shuffle(ticket_generator);
+        }
         tickets_array.push(this.ticket);
+        const pushToJSON = [max_capacity, current_capacity, tickets_array]
+        
+        // Pushea los datos nuevos del array "tickets_array" al archivo JSON y así queda actualizado
+        const dataParse = JSON.stringify(pushToJSON, null, 2);
+        fs.writeFileSync(ruta, dataParse, 'utf8');
 
-        console.log("Your purchase has been made",this.name);
+
+        console.log("Your purchase has been made",(this.name).toUpperCase());
         console.log("Your ticket code is: ",this.ticket);
         console.log("Your seat name is: ",this.seat);
     }
@@ -136,7 +164,7 @@ while(z ==="y"){
     let selection = prompt("Select option: ");
 
     while(isNaN(selection)=== true || selection < 1 || selection > 5){
-        selection = prompt("Select a valid option please");
+        selection = prompt("Select a valid option please: ");
     }
 
     switch(selection){
